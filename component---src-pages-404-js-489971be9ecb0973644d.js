20a10137944a0e3c48dc2bc8b9f0a157127032bb
FatihHyusein <FatihHyusein@users.noreply.github.com>
2022-04-14 21:20:41 +0000

    deployed via Gatsby Publish Action 🎩 for b08784f47d09f57cd1c121ae359cded8ab3bfc11

---
+++ component---src-pages-404-js-489971be9ecb0973644d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklegalwise=self.webpackChunklegalwise||[]).push([[883],{429:function(e,t,l){l.r(t);var n=l(7294),a=l(2282);t.default=function(){return n.createElement(a.Z,null,n.createElement("div",{id:"main"},n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-489971be9ecb0973644d.js.map
